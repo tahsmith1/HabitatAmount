3e50c10be482ccbb7589f3fceff5089556190485
Thomas Smith <tahsmith@hawaii.edu>
2017-11-15 16:16:47 -0500

    Added loop from main script and inserted far edge distance matrices calculations

--- Scripts/BufferCode.R
+++ Scripts/BufferCode.R
@@ -10,5 +10,42 @@ dist.alpha.2<-ifelse(dist.edge.cent.2>meandist,0,1)#movement dist
 diag(dist.alpha.2)<-0 #doesn't include focal patch
 
 
+
+#make lists for each plot for distance/area
+dist.matrix.2015=list()#from patch centers
+distedge.matrix.2015=list()#from patch edges (approximate)
+distfaredge.matrix.2015=list() #center to patch far edge distance matrix
+area.2015=list()
+xy.2015=list()
+plotID.2015=list()
+patchID.2015=list()
+
+#2015
+for(i in 1:17){  
+  plot.i<-subset(coordinates, plot==i & posttrt=="Y")
+  #remove NA locations posttrt
+  plot.i<-plot.i[complete.cases(plot.i[,"East"]),]
+  #plot.i<-subset(plot.i, dead.!="Y")#remove dead patches in 2015 (dead by fall; area 2014 measurements at end of season)
+  patchID.2015[[i]]<-plot.i$patch
+  plotID.2015[[i]]<-plot.i$plot
+  
+  #calculate/store dist/area
+  dist.matrix.2015[[i]]<-as.matrix(dist(cbind(plot.i$East,plot.i$North))) #Euclidean distance matrix
+  area.2015[[i]]<-plot.i$AreaMean2
+  
+  #approximate edge to edge distance
+  radius.i<-(plot.i$AreaMean/pi)^0.5
+  distedge.matrix.2015[[i]]<-dist.matrix.2015[[i]]-outer(radius.i,radius.i,"+")
+  xy.2015[[i]]<-cbind(plot.i$East,plot.i$North)
+  
+  #Distance matrices to far edge
+  distfaredge.matrix.2015[[i]] <- sweep(dist.matrix.2015[[i]], 2, radius.i, "+")
+  diag(distfaredge.matrix.2015[[i]])<-0 #doesn't include focal patch
+}
+
+
+
 ######Prop area
+
+
 
