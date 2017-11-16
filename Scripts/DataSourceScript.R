b13389b408e80d379d2ffe130e335a18eff3ef10
Thomas Smith <tahsmith@hawaii.edu>
2017-11-16 17:42:53 -0500

    Created a script that can be sourced and will update and create all necessary data objects

---
+++ Scripts/DataSourceScript.R
@@ -0,0 +1,181 @@
+#################Script for calculations###########################
+
+library(reshape2)
+library(plyr)
+library(ggplot2)
+library(survival) 
+library(MuMIn)
+library(lme4)
+library(glmmADMB)#zero-inflated mixed model
+
+
+################
+
+load("Data/habitat_amount_2014_2016.RData")
+
+
+#make lists for each plot for distance/area
+dist.matrix.2015=list()#from patch centers
+distedge.matrix.2015=list()#from patch edges (approximate)
+distfaredge.matrix.2015=list() #center to patch far edge distance matrix
+area.2015=list()
+xy.2015=list()
+plotID.2015=list()
+patchID.2015=list()
+
+#2015
+for(i in 1:17){  
+  plot.i<-subset(coordinates, plot==i & posttrt=="Y")
+  #remove NA locations posttrt
+  plot.i<-plot.i[complete.cases(plot.i[,"East"]),]
+  #plot.i<-subset(plot.i, dead.!="Y")#remove dead patches in 2015 (dead by fall; area 2014 measurements at end of season)
+  patchID.2015[[i]]<-plot.i$patch
+  plotID.2015[[i]]<-plot.i$plot
+  
+  #calculate/store dist/area
+  dist.matrix.2015[[i]]<-as.matrix(dist(cbind(plot.i$East,plot.i$North))) #Euclidean distance matrix
+  area.2015[[i]]<-plot.i$AreaMean2
+  
+  #approximate edge to edge distance
+  radius.i<-(plot.i$AreaMean/pi)^0.5
+  distedge.matrix.2015[[i]]<-dist.matrix.2015[[i]]-outer(radius.i,radius.i,"+")
+  xy.2015[[i]]<-cbind(plot.i$East,plot.i$North)
+  
+  #Distance matrices to far edge
+  distfaredge.matrix.2015[[i]] <- sweep(dist.matrix.2015[[i]], 2, radius.i, "+")
+  diag(distfaredge.matrix.2015[[i]])<-0 #doesn't include focal patch
+}
+
+#2014
+dist.matrix.2014=list()
+distfaredge.matrix.2014=list() #center to patch far edge distance matrix
+area.2014=list()
+xy.2014=list()
+plotID.2014=list()
+patchID.2014=list()
+
+for(i in 2:17){  
+  if (i==12) next
+  plot.i<-subset(coordinates, plot==i)
+  #remove NA locations
+  plot.i<-plot.i[complete.cases(plot.i[,c("area","East")]),]
+  patchID.2014[[i]]<-plot.i$patch
+  plotID.2014[[i]]<-plot.i$plot
+  
+  #calculate/store dist/area
+  dist.matrix.2014[[i]]<-as.matrix(dist(cbind(plot.i$East,plot.i$North))) #Euclidean distance matrix
+  area.2014[[i]]<-plot.i$AreaMean2
+  xy.2014[[i]]<-cbind(plot.i$East,plot.i$North)
+  
+  #Distance matrices to far edge
+  radius.i<-(plot.i$AreaMean2/pi)^0.5
+  distfaredge.matrix.2014[[i]] <- sweep(dist.matrix.2014[[i]], 2, radius.i, "+")
+  diag(distfaredge.matrix.2014[[i]])<-0 #doesn't include focal patch
+}
+
+##############################
+
+###Calculate the buffer area based on the far edges of patches (most conservative way to calculate)
+##2015
+bufferfararea2015=list()
+for(i in 2:17){  
+  if (i==12) next
+  
+  #area/dist
+  
+  dist.i<-distfaredge.matrix.2015[[i]]
+  area.i<-area.2015[[i]]
+  
+  #distance metrics
+  dist.inv<-1/dist.i
+  diag(dist.inv)<-0
+  
+  #buffer area
+  dist.alpha<-ifelse(dist.i>meandist,0,1)#movement dist
+  diag(dist.alpha)<-0 #doesn't include focal patch
+  bufferfararea2015[[i]]<-rowSums(sweep(dist.alpha, 2, area.i, "*"))  
+}
+
+bufferfararea2014=list()
+##2014
+for(i in 2:17){  
+  if (i==12) next
+  
+  #area/dist
+  
+  dist.i<-distfaredge.matrix.2014[[i]]
+  area.i<-area.2014[[i]]
+  
+  #distance metrics
+  dist.inv<-1/dist.i
+  diag(dist.inv)<-0
+  
+  #buffer area
+  dist.alpha<-ifelse(dist.i>meandist,0,1)#movement dist
+  diag(dist.alpha)<-0 #doesn't include focal patch
+  bufferfararea2014[[i]]<-rowSums(sweep(dist.alpha, 2, area.i, "*"))  
+}
+
+
+#-------------------------------------------#
+#link metrics with survey data
+#-------------------------------------------#
+
+landscape2015.df<-data.frame(Plot=unlist(plotID.2015),Patch=unlist(patchID.2015),
+                             patcharea=unlist(area.2015),
+                             nnd=unlist(nnd2015), 
+                             bufferarea=unlist(bufferarea2015),
+                             bufferfararea=unlist(bufferfararea2015),
+                             ifm=unlist(ifm2015))
+landscape2014.df<-data.frame(Plot=unlist(plotID.2014),Patch=unlist(patchID.2014),
+                             patcharea=unlist(area.2014),
+                             nnd=unlist(nnd2014), 
+                             bufferarea=unlist(bufferarea2014), 
+                             bufferfararea=unlist(bufferfararea2014),
+                             ifm=unlist(ifm2014))
+landscape.df<-rbind(landscape2015.df, landscape2014.df)
+
+
+
+community<-merge(community, landscape.df, by=c("Plot","Patch"), all=F)
+names(community)[names(community) == "patcharea.x"] <- "patcharea"
+names(community)[names(community) == "nnd.x"] <- "nnd"
+names(community)[names(community) == "bufferarea.x"] <- "bufferarea"
+names(community)[names(community) == "ifm.x"] <- "ifm"
+
+community$tbufferfararea<-community$patcharea + community$bufferfararea
+
+#####################Calculation of subeseted plot to avoid edge effects
+####Calculations
+
+pc_minmax <- read.csv("Data/plot_corners_minmax.csv")
+
+pc_minmax$subXmin <- pc_minmax$Xmin + meandist
+pc_minmax$subXmax <- pc_minmax$Xmax - meandist
+pc_minmax$subYmin <- pc_minmax$Ymin + meandist
+pc_minmax$subYmax <- pc_minmax$Ymax - meandist
+
+inmax <- subset(pc_minmax, Plot==1)
+
+coordinates$coordInc <- NA
+head(coordinates)
+
+subcoordInd = list()
+
+for(i in 1:17){  
+  plot.i<-subset(coordinates, plot==i)
+  c_minmax <- subset(pc_minmax, Plot==i)
+  subcoordInd[[i]]<- ifelse(plot.i$East > c_minmax$subXmin & plot.i$East < c_minmax$subXmax & 
+                              plot.i$North > c_minmax$subYmin & plot.i$North < c_minmax$subYmax,1,0)
+  
+}
+################
+coordinates$coordInc <- unlist(subcoordInd)
+
+subsetmerge<- coordinates[,c(1,2,15)]
+names(subsetmerge)[1:2] <- c("Plot","Patch")
+
+communityNew <-  merge(community2015.nocont,subsetmerge , by=c("Plot","Patch"), all=F)
+communitySub <- subset(communityNew, coordInc == 1)
+
+
