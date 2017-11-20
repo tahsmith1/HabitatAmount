b3a80ebac1b1eb181583267d10f4a467062cc14a
Thomas Smith <tahsmith@hawaii.edu>
2017-11-20 17:52:07 -0500

    Added some analyses and fixed period in object name

--- Scripts/glms.R
+++ Scripts/glms.R
@@ -3,15 +3,27 @@
 
 community<- read.csv("ManData/community.csv")
 community2015 <- read.csv('ManData/community2015.csv')
-community2015nocont <- read.csv("ManData/community2015nocont.csv")
+community2015.nocont <- read.csv("ManData/community2015nocont.csv")
 communitySub <- read.csv("ManData/communitySub.csv")
 coordinates<-  read.csv("ManData/coordinates.csv")
 
 
 ##############Basic Binomial regression
 
-community2015nocont$CheliBinary <-ifelse(community2015nocont$Cheli.adults >0, 1,0)
+community2015.nocont$CheliBinary <-ifelse(community2015.nocont$Cheli.adults >0, 1,0)
 
-glm_patchareaXbufferarea <- glm(CheliBinary~bufferarea*patcharea,family = binomial(link = logit),data = community2015nocont)
+glm_patchareaXbufferarea <- glm(CheliBinary~bufferarea*patcharea,family = binomial(link = logit),data = community2015.nocont)
+
+glm_nndXbufferarea <- glm(CheliBinary~bufferarea*nnd,family = binomial(link = logit),data = community2015.nocont)
+
+
 
 summary(glm_patchareaXbufferarea)
+summary(glm_nndXbufferarea)
+
+
+cor(community2015.nocont$patcharea, community2015.nocont$bufferarea)  ### correlation
+
+
+plot(glm_patchareaXbufferarea)
+
