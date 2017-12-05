50dd2e3bf972ea1ae3fd42ac82cfbf444e20202d
Thomas Smith <tahsmith@hawaii.edu>
2017-12-05 14:43:59 -0500

    Moved pre analysis data manipulation into its own script that can be sourced before running analysis only

--- Scripts/ZipAnalysis.R
+++ Scripts/ZipAnalysis.R
@@ -1,85 +1,10 @@
-####Reads in already calculated community and coordinate data frames#######
 
-
-community<- read.csv("ManData/community.csv")
-#community2015 <- read.csv('ManData/community2015.csv')
-#community2015.nocont <- read.csv("ManData/community2015.nocont.csv")
-#communitySub <- read.csv("ManData/communitySub.csv")
-landscape2014 <- read.csv("ManData/landscape2014.csv")
-landscape2015 <- read.csv("ManData/landscape2015.csv")
-
-#################
-
-treatprop<- unique(community[,c("Plot","Patch","TrtType","PropLost")])
-
-############ subset by Aug-Nov
-
-comm2014<- subset(community, subset = community$year == 2014)
-comm2015<- subset(community, subset = community$year == 2015)
-
-comm2014an <- subset(comm2014, comm2014$month >7)
-comm2015an <- subset(comm2015, comm2015$month >7)
-
-#commlate <- rbind(comm2014an,comm2015an)
-
-##########
-#str(treatprop)
-lnd2014<- landscape2014[,-1]
-
-landsc2014<- merge(lnd2014,treatprop, by= c("Plot","Patch"), all = F)
-
-#mergedcomm2014 <- aggregate(Cheli.adults~Plot+Patch, data= comm2014an, FUN= sum)
-#comm2014m<- merge(mergedcomm2014,landsc2014, by = c("Plot", "Patch"), all = F)
-
-lnd2015<- landscape2015[,-1]
-landsc2015<- merge(lnd2015,treatprop, by= c("Plot","Patch"), all = F)
-
-#mergedcomm2015 <- aggregate(Cheli.adults~Plot+Patch, data= comm2015an, FUN= sum)
-#comm2015m<- merge(mergedcomm2015,landsc2015, by = c("Plot", "Patch"), all = F)
-
-####an3 in subsetobervation.R
-comhold = list()
-chold = list()
-library(data.table)
-
-for(i in 2:17){  
-  if (i==12) next
-  comm.i <- subset(comm2015an, comm2015an$Plot == i)
-  chold = list()
-  for(ii in  1:length(unique(comm.i$Patch))){
-    pcomm.ii <- subset(comm.i, comm.i$Patch ==  unique(comm.i$Patch)[ii])
-    pcomm.ii <- pcomm.ii[order(pcomm.ii$SurveyNumber),]
-    chold[[ii]]<- pcomm.ii[c(1,3,5),]
-  }
-  comhold[[i]] <- rbindlist(chold)
-  
-}
-
-comm2015an3<- rbindlist(comhold)
-#################
-
-#communityYear<- rbind(comm2014an,comm2015an3)
-
-leftpatches <- comm2015an3[,c("Patch","Plot")]
-leftpatches<- unique(leftpatches)
-comm2014ms <- merge(comm2014an, leftpatches, by = c("Plot", "Patch"), all= F)
-
-communityYear<- rbind(comm2014ms,comm2015an3)
-
-communityYear$Plot<- as.factor(communityYear$Plot)
-communityYear$Patch<- as.factor(communityYear$Patch)
-communityYear$year<- as.factor(communityYear$year)
-
-comm.nocont<- subset(communityYear, TrtType != "cont")
-#########
-subsetmerge <- read.csv("ManData/subsetmerge.csv")
-communityNew <-  merge(comm.nocont,subsetmerge , by=c("Plot","Patch"), all=F)
-comm.sub <- subset(communityNew, coordInc == 1)
-
-#######
+########Begin analyses after sourcing pre analysis data manipulations 
 
 library(pscl)
 library(glmmADMB)
+
+source("Scripts/PreAnalysisSource.R")
 
 #############
 
