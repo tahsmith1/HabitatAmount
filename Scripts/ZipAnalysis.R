b0cd4c5b6a15bcaa49c4920a6bf684ae21bd7320
Thomas Smith <tahsmith@hawaii.edu>
2017-12-05 14:49:20 -0500

    Removed old, incorrect analyses

--- Scripts/ZipAnalysis.R
+++ Scripts/ZipAnalysis.R
@@ -15,42 +15,6 @@ cor(comm.nocont$bufferfararea,comm.nocont$patcharea)
 
 cor(comm.nocont$bufferarea,comm.nocont$patcharea+comm.nocont$bufferarea)
 ####################Testing patch area and nnd effects against bufferarea across treatments
-zPatch<-zeroinfl(Cheli.adults ~ patcharea*year*TrtType | 1, data = comm.nocont)
-zBuffer<-zeroinfl(Cheli.adults ~ bufferarea*year*TrtType | 1, data = comm.nocont)
-zPatchBuffer<-zeroinfl(Cheli.adults ~ patcharea*bufferarea*year*TrtType | 1, data = comm.nocont)
-zNNDBuffer<-zeroinfl(Cheli.adults ~ bufferarea*nnd*year*TrtType | 1, data = comm.nocont)
-zNND<-zeroinfl(Cheli.adults ~ nnd*year*TrtType | 1, data = comm.nocont)
-zNNDPatch<-zeroinfl(Cheli.adults ~ nnd*patcharea*year*TrtType | 1, data = comm.nocont)
-#zNNDPatchBuffer <-zeroinfl(Cheli.adults ~ nnd*patcharea*bufferarea*year*TrtType | 1, data = comm.nocont)  NA groups
-
-summary(zPatch)
-summary(zBuffer)
-summary(zNND)
-summary(zPatchBuffer)
-summary(zNNDBuffer)
-
-AIC(zPatch,zBuffer,zNND,zPatchBuffer,zNNDBuffer, zNNDPatch,zNNDPatchBuffer)
-
-
-###########################Farbuffer area differences
-
-zPatch<-zeroinfl(Cheli.adults ~ patcharea*year*TrtType | 1, data = comm.nocont)
-zBuffer<-zeroinfl(Cheli.adults ~ bufferfararea*year*TrtType | 1, data = comm.nocont)
-zPatchBuffer<-zeroinfl(Cheli.adults ~ patcharea*bufferfararea*year*TrtType | 1, data = comm.nocont)
-zNNDBuffer<-zeroinfl(Cheli.adults ~ bufferfararea*nnd*year*TrtType | 1, data = comm.nocont)
-zNND<-zeroinfl(Cheli.adults ~ nnd*year*TrtType | 1, data = comm.nocont)
-zNNDPatch<-zeroinfl(Cheli.adults ~ nnd*patcharea*year*TrtType | 1, data = comm.nocont)
-#zNNDPatchBuffer <-zeroinfl(Cheli.adults ~ nnd*patcharea*bufferarea*year*TrtType | 1, data = comm.nocont)  NA groups
-
-summary(zPatch)
-summary(zBuffer)
-summary(zNND)
-summary(zPatchBuffer)
-summary(zNNDBuffer)
-
-AIC(zPatch,zBuffer,zNND,zPatchBuffer,zNNDBuffer, zNNDPatch)
-
-#############################################
 
 zPatch<-zeroinfl(Cheli.adults ~ patcharea*year*TrtType | 1, data = comm.sub)
 zBuffer<-zeroinfl(Cheli.adults ~ bufferfararea*year*TrtType | 1, data = comm.sub)
@@ -61,36 +25,23 @@ zNNDPatch<-zeroinfl(Cheli.adults ~ nnd*patcharea*year*TrtType | 1, data = comm.s
 
 AIC(zPatch,zBuffer,zNND,zPatchBuffer,zNNDBuffer, zNNDPatch)
 
-summary(zPatch)
-summary(zBuffer)
-summary(zNND)
-summary(zPatchBuffer)
-summary(zNNDBuffer)
 
 zmPatchBuffer<-glmmadmb(Cheli.adults~scale(bufferarea)*scale(patcharea)*TrtType*year+(1|Plot), data=comm.sub, family="Poisson", zeroInflation=T)
 summary(zmPatchBuffer)
 
 
-zPatch<-zeroinfl(Cheli.adults ~ patcharea*year*TrtType | 1, data = comm.sub)
-zBuffer<-zeroinfl(Cheli.adults ~ bufferfararea*year*TrtType | 1, data = comm.sub)
-zPatchBuffer<-zeroinfl(Cheli.adults ~ patcharea*bufferfararea*year*TrtType | 1, data = comm.sub)
-zNNDBuffer<-zeroinfl(Cheli.adults ~ bufferfararea*nnd*year*TrtType | 1, data = comm.sub)
-zNND<-zeroinfl(Cheli.adults ~ nnd*year*TrtType | 1, data = comm.sub)
-zNNDPatch<-zeroinfl(Cheli.adults ~ nnd*patcharea*year*TrtType | 1, data = comm.sub)
-
-zPatch<-zeroinfl(Cheli.adults ~ patcharea | 1, data = comm.sub)
-zBuffer<-zeroinfl(Cheli.adults ~ bufferarea+patcharea | 1, data = comm.sub)
-
-####################New Actual testing patch area and nnd effects against bufferarea across treatments
+####################Testing patch area and nnd effects against bufferarea across treatments using unsubsetted data
 zPatch<-zeroinfl(Cheli.adults ~ patcharea | 1, data = comm.nocont)
 zBuffer<-zeroinfl(Cheli.adults ~ patcharea + bufferarea | 1, data = comm.nocont)
 zNNDBuffer<-zeroinfl(Cheli.adults ~ bufferarea*nnd | 1, data = comm.nocont)
 zNND<-zeroinfl(Cheli.adults ~ nnd | 1, data = comm.nocont)
 zPropTrt<-zeroinfl(Cheli.adults ~PropLost*TrtType | 1, data = comm.nocont)
-comm.nocont$PropLost
+
 
 AICtable <- AIC(zPatch,zBuffer,zNNDBuffer,zNND,zPropTrt)
 AICtable[order(AICtable$AIC),]
+
+######ZIP mixed models using final dataset 
 
 zmBuffer<-glmmadmb(Cheli.adults~scale(bufferfararea)*scale(patcharea)+(1|Plot), data=comm.sub, family="Poisson", zeroInflation=T)
 zmPatchBuffer<-glmmadmb(Cheli.adults~scale(patcharea)+scale(bufferfararea)+(1|Plot), data=comm.sub, family="Poisson", zeroInflation=T)
@@ -136,6 +87,8 @@ ggplot(comm.sub, aes(x = year, y= log(nnd)))+
   xlab("Year") +
   ylab("Nearest neighbor distance")
 
+####Adding year interaction into the analysis (picks up differences esp for trtmenttype)
+
 mPatchBuffer<-glmmadmb(Cheli.adults~scale(patcharea)+scale(bufferarea)*year+(1|Plot), data=comm.sub, family="Poisson", zeroInflation=T)
 mPatch<-glmmadmb(Cheli.adults~scale(patcharea)+(1|Plot), data=comm.sub, family="Poisson", zeroInflation=T)
 mNND<-glmmadmb(Cheli.adults~scale(patcharea)+scale(nnd)*year+(1|Plot), data=comm.sub, family="Poisson", zeroInflation=T)
@@ -147,13 +100,7 @@ mPropTrt<-glmmadmb(Cheli.adults~scale(patcharea)+PropLost*TrtType*year+(1|Plot),
 AICtable <- AIC(mPatch,mPatchBuffer,mNND,mNNDBuffer ,mPropTrt)
 
 summary(mNNDBuffer)
+summary(mPropTrt)
 
-mean(scale(comm.sub$patcharea))
-
-
-pred_nndbuffer = data.frame(bufferarea = seq(min(scale(comm.sub$bufferarea)), max(scale(comm.sub$bufferarea)), length.out = 200)) 
-pred_nndbuffer$nnd = median(comm.sub$nnd)
-pred_nndbuffer$patcharea = median(scale(comm.sub$patcharea))
-predict(mNNDBuffer, pred_nndbuffer, type = "response")
 
 
